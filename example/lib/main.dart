4d05eef28afe30c2e0aa802b0a50ea2060b5d819
salim lachdhaf <salimlachdef@gmail.com>
2020-05-08 16:06:13 +0100

    first publish on pub.dev

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:ftpconnect/ftpConnect.dart';
+import 'package:ftpconnect/ftpconnect.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(MyApp());
